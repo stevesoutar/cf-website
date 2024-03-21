67a0415cc25bb3ded4d88d51646e8ff587d6900e
Craig Soutar <bodsda@ubuntu.com>
2024-03-21 14:11:40 +0000

    intial

--- backend.tf
+++ backend.tf
@@ -1,9 +1,9 @@
 terraform {
   backend "s3" {
     encrypt        = true
-    bucket         = "my-org-tf-state"
-    dynamodb_table = "my-org-tf-state-lock"
-    key            = "my-static-website.tfstate"
-    region         = "us-east-1"
+    bucket         = "cf-website-backend"
+    dynamodb_table = "cf-website-backend-state-lock"
+    key            = "cf-website-backend.tfstate"
+    region         = "eu-west-1"
   }
 }
